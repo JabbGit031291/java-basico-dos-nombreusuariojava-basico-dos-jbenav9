ca94a127c6982fc931871c3e3f5c8e1555e5816f
Benavides Benavides <JBENAV9@bancodebogota.com.co>
2024-08-08 17:19:57 -0500

    segundojava2

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+
+public class Main {
+    public static void main(String[] args) {
+        /* ejer1  */
+        /*
+        int numero1=1;
+        int numero2=2;
+        int numero3=3;
+        int numero4=4;
+        int numero5=5;
+        int numero6=6;
+        int numero7=7;
+        int numero8=8;
+        int numero9=9;
+        int numero10=10;
+        int sumanumeros;
+
+        sumanumeros = numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10;
+        System.out.println("las suma de los numeros enteros son " + sumanumeros);
+
+
+    }
+*/
+        /* ejer2  */
+/*
+        int num = 0;
+        int sum = 0;
+
+
+        for (int i=0; i<10; i++){
+
+            num = num + 1;
+            sum = sum + num;
+            System.out.println("las suma de los numeros enteros son " + sum);
+        }
+*/
+
+        /* ejer3  */
+   /*
+        int numeros = 0;
+        int suma = 0;
+
+        while (numeros < 11) {
+            suma = suma + numeros;
+            numeros++;
+        }
+        System.out.println("La suma de los numeros es " + suma);
+    }
+    */
+
+    }
+}
+
+
+
